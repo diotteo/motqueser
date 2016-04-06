28c14e0c96b294987729295b3f7de6a96d3e4897
Olivier Diotte <olivier@diotte.ca>
2016-04-05 23:34:12 -0400

    Add ItemNotFoundException class

---
+++ src/ItemNotFoundException.java
@@ -0,0 +1,11 @@
+package ca.dioo.java.motqueser;
+
+public class ItemNotFoundException extends Exception {
+	public ItemNotFoundException(String message) {
+		super(message);
+	}
+
+	public ItemNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
